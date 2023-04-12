59807fc756584ecd0e16725ac02b1f715bbb7070
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-04-12 17:00:31 +0530

    Added Form component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import HomePage from "./Components/HomePage";
+import FormComp from "./Components/FormComp";
+import CardsGrid from "./Components/CardsComp"
 
 const App = () => {
   return (
     <>
       <HomePage />
+      <FormComp />
+      <CardsGrid />
     </>
   );
 };
